5750ad62c1110db4819f92e732efad219630d5cd
Alain Lehmann <alain.lehmann@gmail.com>
2020-02-20 23:13:39 +0100

    demonstrate that the ruby shell is working

--- shell.nix
+++ shell.nix
@@ -20,12 +20,28 @@ let
   rubix = pkgs.writeScriptBin "rubix" ''
     ${bundler}/bin/bundle && ${bundix}/bin/bundix
   '';
+  demonstrateSuccess = pkgs.writeScriptBin "demo" ''
+    echo "the following should print some like"
+    echo "    require rbtree => true, and not an error"
+    echo "Demo starting now..."
+    echo ""
+    echo "require 'rbtree'" | irb
+  '';
 in stdenv.mkDerivation {
   name = "ruby-sample";
   buildInputs = [
     bundler bundix rubix
     ruby
-    # custom environemnt.. require ``rubix`` to be run before uncomenting
-    #rubyEnvironment
+    # custom environemnt.. generated with rubix, aka bundle && bundix
+    rubyEnvironment
+
+    # sample ruby session to demonstrate the gem is available
+    # simply run ``demo`` in a nix-shell or direnv environment
+    demonstrateSuccess
   ];
+
+  shellHook = ''
+    # an extra goodie, this will be run when entering the nix-shell
+    ${demonstrateSuccess}/bin/demo
+  '';
 }
