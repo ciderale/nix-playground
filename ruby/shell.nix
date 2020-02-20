225ebefbd47a6c14284a656efa6278f3ce81c882
Alain Lehmann <alain.lehmann@gmail.com>
2020-02-20 23:51:14 +0100

    add extensive documentation

--- ruby/shell.nix
+++ ruby/shell.nix
@@ -1,15 +1,24 @@
 let
+  # define an overlay to customize packages
   overlay = self: super: {
     # select your ruby version of choice
-    # that affect all ruby related things in nixpkgs
+    # that affects all ruby related things in nixpkgs
     ruby = self.ruby_2_5;
   };
-  pkgs = import ./nixpkgs.nix {overlays=[overlay];};
+
+  # use a "frozen" version of nixpkgs (rather than <nixpkgs>)
+  pkgs = import ./nixpkgs.nix {
+    # apply the overlay to nixpkgs
+    overlays = [overlay];
+  };
 in
 
+# "with" move all packages (of 'pkgs') in scope, to avoid boilerplate
+# this allows to write 'ruby' instead of 'pkgs.ruby'
 with pkgs;
 
 let
+  # generate a ruby environment with all gems defined in Gemfile
   rubyEnvironment = bundlerEnv {
     name = "tmp-bundler-env";
     inherit ruby;
@@ -17,9 +26,14 @@ let
     lockfile = ./Gemfile.lock;  # generated with ``bundle``
     gemset   = ./gemset.nix;    # generated with ``bundix``
   };
+
+  # combine the two commands for simpler regeneration
+  # writeScriptBin creates an executable script named "rubix"
   rubix = pkgs.writeScriptBin "rubix" ''
     ${bundler}/bin/bundle && ${bundix}/bin/bundix
   '';
+
+  # a "demo" script to know that ruby finds the gem
   demonstrateSuccess = pkgs.writeScriptBin "demo" ''
     echo "the following should print some like"
     echo "    require rbtree => true, and not an error"
@@ -27,15 +41,29 @@ let
     echo ""
     echo "require 'rbtree'" | irb
   '';
-in stdenv.mkDerivation {
+
+in
+
+# a brand new derivation (akin package) with the ruby shell
+stdenv.mkDerivation {
   name = "ruby-sample";
+
+  # all derivations in buildInputs are made available to nix-shell
+  # this places any /bin folder of those packages onto the path
+  # hence ruby and the previously defined script are readily available
   buildInputs = [
+
+    # dev dependencies to install the gems
     bundler bundix rubix
+
+    # the ruby interpreter to be used
     ruby
-    # custom environemnt.. generated with rubix, aka bundle && bundix
+
+    # custom ruby environemnt as defined by the Gemfile
+    # lock and nix file generated with rubix, aka bundle && bundix
     rubyEnvironment
 
-    # sample ruby session to demonstrate the gem is available
+    # provide a simple script to check that it is all working
     # simply run ``demo`` in a nix-shell or direnv environment
     demonstrateSuccess
   ];
@@ -43,5 +71,9 @@ in stdenv.mkDerivation {
   shellHook = ''
     # an extra goodie, this will be run when entering the nix-shell
     ${demonstrateSuccess}/bin/demo
+    demo # non-absolute paths works, because of buildInputs!
+
+    # sometimes convenient to reference the project top-level folder
+    export PROJECT_DIR=$(pwd)
   '';
 }
