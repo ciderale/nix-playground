12eb9476347a6277b184fdd875bd783dffcffcfe
Alain Lehmann <alain.lehmann@gmail.com>
2020-02-25 00:03:34 +0100

    move packageJsonOrLock into overlay

--- node/shell.nix
+++ node/shell.nix
@@ -13,17 +13,17 @@ let
       rm -rf ./node_modules # node2nix complains as node_modules interfer
       ${self.njsPkgs}/bin/node2nix -d --nodejs-${nodeVersion} -l package-lock.json
     '';
+
+    # filter to retain only package.json and its lock file
+    # this is minimal, when only npm depenency shall be managed
+    onlyPackageJsonOrLock = path: _:
+      let baseName = baseNameOf (toString path);
+       in baseName == "package.json" || baseName == "package-lock.json";
   };
 
   # load your favourite nixpkgs set and select the nodejs version
   # NOTE: select the nodejs version you like to use
   pkgs = import ../nixpkgs.nix { overlays = [(selectNodejs 12)]; };
-
-  # filter to retain only package.json and its lock file
-  # this is minimal, when only npm depenency shall be managed
-  onlyPackageJsons = path: _:
-    let baseName = baseNameOf (toString path);
-     in baseName == "package.json" || baseName == "package-lock.json";
 
   # load the node2nix generated default.nix file
   n2n = import ./. { inherit pkgs; };
@@ -37,7 +37,7 @@ let
 
     # ignore everything, but the package dependency definition
     # we only fetch dependencies, don't package the project for nix
-    src = builtins.filterSource onlyPackageJsons ./.;
+    src = builtins.filterSource pkgs.onlyPackageJsonOrLock ./.;
   };
 in
 
