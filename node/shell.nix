2c6ebf4cb04580637dd243226de56b428bd843a6
Alain Lehmann <alain.lehmann@gmail.com>
2020-02-24 23:19:08 +0100

    improve documentation of shell.nix
    - buildInput np.shell is not needed!
    => shellHook contains all information

--- node/shell.nix
+++ node/shell.nix
@@ -1,29 +1,44 @@
 let
+  # load your favourite nixpackage set
   pkgs = import ../nixpkgs.nix {};
+
+  # load the node2nix generated default.nix file
   n2n = import ./. {pkgs=pkgs;};
 
+  # filter to retain only package.json and its lock file
   onlyPackageJsons = path: _:
     let baseName = baseNameOf (toString path);
      in baseName == "package.json" || baseName == "package-lock.json";
 
+  # override to slightly customize the generated node project dependencies
   np = n2n // {
     shell = n2n.shell.override {
+      # buildInput needed to build webpack
       buildInputs = with pkgs.darwin.apple_sdk.frameworks; [CoreServices];
+
+      # ignore everything, but the package dependency definition
+      # we only fetch dependencies, don't package the project for nix
       src = builtins.filterSource onlyPackageJsons ./.;
     };
   };
 
   # the commoand to generate nix expressions from package-lock.json
   nodix = pkgs.writers.writeBashBin "nodix" ''
-    rm -rf ./node_modules
+    rm -rf ./node_modules # node2nix complains as node_modules interfer
     node2nix -d --nodejs-12 -l package-lock.json
   '';
 in
+
 with pkgs;
 
 mkShell {
+  # inherit the shellHook (and its nodeDependency)
   inherit (np.shell) shellHook nodeDependencies;
-  buildInputs = [nodejs-12_x nodePackages_12_x.node2nix
-   nodix np.shell
+
+  buildInputs = [
+    # the node version to be used
+    nodejs-12_x nodePackages_12_x.node2nix
+    # our little helper script
+    nodix
   ];
 }
