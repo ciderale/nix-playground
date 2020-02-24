aa5a69a9ce1095f0d237d94564de42b58a93d65c
Alain Lehmann <alain.lehmann@gmail.com>
2020-02-24 19:50:56 +0100

    document command to run node2nix

--- node/shell.nix
+++ node/shell.nix
@@ -1,10 +1,15 @@
 let
   pkgs = import ../nixpkgs.nix {};
   n2n = import ./. {pkgs=pkgs;};
+
+  # the commoand to generate nix expressions from package-lock.json
+  updateNix = pkgs.writers.writeBashBin "updateNix" ''
+    node2nix --nodejs-12 -l package-lock.json
+  '';
 in
 with pkgs;
 
 mkShell {
   inherit (n2n.shell) shellHook nodeDependencies;
-  buildInputs = [nodejs-12_x];
+  buildInputs = [nodejs-12_x nodePackages_12_x.node2nix updateNix];
 }
