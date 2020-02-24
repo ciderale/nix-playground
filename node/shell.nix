46e5f1f53640df8f9605aaf15d8e229d87ea5bbe
Alain Lehmann <alain.lehmann@gmail.com>
2020-02-24 23:52:21 +0100

    add overlay for nodejs version selection

--- node/shell.nix
+++ node/shell.nix
@@ -1,17 +1,27 @@
 let
+  nodejsSelect = self: super: rec {
+    # NOTE: select the nodejs version you like to use
+    nodeVersion = "12";
+    njs = super."nodejs-${nodeVersion}_x";
+    njsPkgs = super."nodePackages_${nodeVersion}_x".node2nix;
+
+    # the command to generate nix expressions from package-lock.json
+    # run 'npm install --save[-dev] your depenency' to generate the lock file
+    # run 'nodeix' to generate the necessary nix files replicating the lock
+    nodix = super.writers.writeBashBin "nodix" ''
+      rm -rf ./node_modules # node2nix complains as node_modules interfer
+      ${njsPkgs}/bin/node2nix -d --nodejs-${nodeVersion} -l package-lock.json
+    '';
+  };
+
   # load your favourite nixpkgs set
-  pkgs = import ../nixpkgs.nix {};
+  pkgs = import ../nixpkgs.nix { overlays = [nodejsSelect]; };
 
   # filter to retain only package.json and its lock file
   # this is minimal, when only npm depenency shall be managed
   onlyPackageJsons = path: _:
     let baseName = baseNameOf (toString path);
      in baseName == "package.json" || baseName == "package-lock.json";
-
-  # NOTE: select the nodejs version you like to use
-  nodeVersion = "12";
-  njs = pkgs."nodejs-${nodeVersion}_x";
-  njsPkgs = pkgs."nodePackages_${nodeVersion}_x".node2nix;
 
   # load the node2nix generated default.nix file
   n2n = import ./. { inherit pkgs; };
@@ -27,14 +37,6 @@ let
     # we only fetch dependencies, don't package the project for nix
     src = builtins.filterSource onlyPackageJsons ./.;
   };
-
-  # the command to generate nix expressions from package-lock.json
-  # run 'npm install --save[-dev] your depenency' to generate the lock file
-  # run 'nodeix' to generate the necessary nix files replicating the lock
-  nodix = pkgs.writers.writeBashBin "nodix" ''
-    rm -rf ./node_modules # node2nix complains as node_modules interfer
-    ${njsPkgs}/bin/node2nix -d --nodejs-${nodeVersion} -l package-lock.json
-  '';
 in
 
 with pkgs;
