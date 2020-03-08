43858acf37de2e36e153cde0554efd97c6135643
Alain Lehmann <alain.lehmann@gmail.com>
2020-03-08 21:43:51 +0100

    marp: adding to generate slides

--- shell.nix
+++ shell.nix
@@ -3,11 +3,14 @@ let
   pkgs = import ./nixpkgs.nix {overlays=[selectNodejs];};
 
   buildInputs = [pkgs.darwin.apple_sdk.frameworks.CoreServices];
+
+  marp = pkgs.callNode2nix ./slides/marp { inherit buildInputs; };
 in
 with pkgs;
 mkShell {
   buildInputs = [
     nodix
+    marp.env
   ];
 }
 
