a9e6d6dcbebb767e7d960e300eba4f109e17a7df
Alain Lehmann <alain.lehmann@gmail.comm>
2020-09-21 21:59:40 +0200

    updating pandoc demo slides

--- slides/pandoc/default.nix
+++ slides/pandoc/default.nix
@@ -1,4 +1,4 @@
-{writers, pandoc, entr, fetchFromGitHub}:
+{writers, pandoc, entr, fetchFromGitHub, mkDerivation}:
 
 rec {
 
@@ -9,16 +9,40 @@ rec {
     sha256 = "0hv3kl4x291ifsvjfk95pdnl51fyd164h96rd44mcflsslslqpnx";
   };
 
+  # cleaned up version with all files in the right spot
+  revealJsL = mkDerivation {
+    src = revealJs;
+    name = "reveal.js-dist";
+    installPhase = ''
+      mkdir -p $out;
+      ln -s $src/css $out;
+      ln -s $src/js $out;
+      ln -s $src/plugin $out;
+    '';
+  };
+
+  # build slides using pandoc.
   slides-build = writers.writeBashBin "slides-build" ''
     DOCUMENT=$1
+    if [ ! -r "$DOCUMENT" ]; then
+      echo "usage: $(basename $0) <slides.md> [additional pandoc arguments]"
+      echo ""
+      echo "compiles markdown as reveal js presentation '<slides.md>.html'"
+      exit 1
+    fi
     shift
-    RJS=${revealJs}
-    ln -sfT $RJS ./.rjs
-    ${pandoc}/bin/pandoc -t revealjs -s -o ''${DOCUMENT}.html "''$DOCUMENT" -V revealjs-url=./.rjs $@
+    ln -sfT ${revealJsL} reveal.js
+    ${pandoc}/bin/pandoc -t revealjs -s -o "$DOCUMENT".html "$DOCUMENT" $@
+  '';
+
+  slides-selfcontained= writers.writeBashBin "slides-selfcontained" ''
+    DOCUMENT=$1; shift
+    ${slides-build}/bin/slides-build $DOCUMENT --self-contained $@
+    # --css ./css/print/paper.css
   '';
 
   slides-pdf = writers.writeBashBin "slides-pdf" ''
-    DOCUMENT=$1
+    DOCUMENT=$1; shift
     HTML=$DOCUMENT.html
     decktape $HTML $DOCUMENT.pdf
   '';
@@ -28,16 +52,18 @@ rec {
     DOCUMENT=$1
     HTML=$DOCUMENT.html
 
+    echo "# watching for output changes and update slides"
     browser-sync start --server --index $HTML --files $HTML --logLevel silent --files **/*.css &
     BSPID=$!
 
+    echo "# watching for input changes and recompile"
     ${entr}/bin/entr -a -n ${slides-build}/bin/slides-build $DOCUMENT <<< $DOCUMENT 2>&1 >/dev/null &
     ENTRPID=$!
 
     trap "kill $BSPID $ENTRPID" EXIT
 
     vim $DOCUMENT
-    ${pandoc}/bin/pandoc -t revealjs -s -o ''${DOCUMENT}.html "''$DOCUMENT" -V revealjs-url=./tmp/reveal.js
+    #${pandoc}/bin/pandoc -t revealjs -s -o ''${DOCUMENT}.html "''$DOCUMENT" -V revealjs-url=./tmp/reveal.js
 
     # create pdf after finishing editing
     # ${slides-pdf}/bin/slides-pdf $DOCUMENT > /dev/null 2&>/dev/null &
--- slides/pandoc/sample.md
+++ slides/pandoc/sample.md
@@ -9,17 +9,49 @@ theme: sky
 
 ## First heading
 
-- bullet points
-
-- more test sample
-
+- Bullet points
+- More Bullet points
 - cool stuff!
 
-<div class="notes">
-Speaker notes!
-</div>
+## Speaker Notes
 
-## More heading
+Add speaker notes with (:::) delimitation
 
-- another content
-- more
+>  ::: notes
+>
+>  your notes go here
+>
+>  important: ":::" go before AND after the notes
+>
+>  :::
+
+::: notes
+
+don't forget the final ":::" after the note
+
+:::
+
+## Adding html directive
+
+* html markup is directly included in the output
+* html comment `<!-- comment -->` can be used
+- This links to <a href="http://google.com">google</a>
+- what is the best way to add styles?
+
+## Inline CSS Editing
+
+* Interesting, but probably not useful
+
+<style contenteditable="true" style=" display: block; background-color: #2d2d2d; color: #0F0; padding: 1em; white-space: pre; width: 100%; overflow: auto; font-size: 10pt; text-align: left">
+  /* Put CSS here and watch your page update as you type */
+  blockquote {
+    background-color: #f00 !important;
+    text-align: left;
+  }
+</style>
+
+::: notes
+
+Nicer speaker notes
+
+:::
